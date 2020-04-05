df5cca764576f86d4c622aa71382c876d323ec8d
Marlon <1362941473@qq.com>
2020-04-05 22:12:43 +0800

    Frame Update
    Wait for API account

---
+++ api.py
@@ -0,0 +1,15 @@
+import TwitterAPI
+
+
+api = TwitterAPI()
+
+def initial(uid):
+    user = api.TwitterResponse()
+    return user
+#Get the tweet content
+def getNewMessage():
+    pass
+
+#IF There is a new tweet
+def noticification():
+    pass
