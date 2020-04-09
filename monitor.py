39728f8e7ba20868276e1e85a90c77cb64ff19ea
Marlon <1724122@i.pkuschool.edu.cn>
2020-04-09 20:31:34 +0800

    nonebot没接 monitor脚本写完了
    实现功能想要用Linux定时运行monitor然后返回参数给nonebot异步上传至8080

---
+++ monitor.py
@@ -0,0 +1,98 @@
+import twint
+import os
+import time
+import datetime
+
+monitor_user = 'nekomataokayu'
+#monitor_user = 'Cyame1121'
+
+class twitterList:
+    def __init__(self,inlist):
+        self.tList = list(inlist)
+        pass
+
+class twitterInfo(object):
+    """docstring fortwitterInfo."""
+
+    def __init__(self, line):
+        self.address, self.date, self.time, self.timezone, self.username, self.content = line.split(' ',5)
+    def __repr__(self):
+        if self.username = "<nekomataokayu>":
+            return("小粥在{0} {1}发布了新推特：{2}\N原推特地址为：{3}".format(self.time,self.date,self.content))
+        else:
+            return("===暂不支持其他用户===")
+    def __str__(self):
+        return __repr__(self)
+
+
+
+def initialUser(monitor_user,today):
+    conf = twint.Config()
+    conf.Username = monitor_user
+    #conf.Store_json = True
+    #conf.Output = "temp.json"
+    conf.Output = "newTweet.txt"
+    #JSON is hard to modify
+    #conf.Store_csv = True
+    #conf.Output = "temp.csv"
+    #conf.Custom["content"] = ["date","time","tweet"]
+    conf.Since = "{0} 00:00:00".format(today)
+    return conf
+
+def getYesterday():
+    yesterday = datetime.date.today() + datetime.timedelta(-1)
+    return yesterday
+
+def get_new_twitter():
+    # empty list
+    newTweet = twitterList([])
+    yesterdayDate = getYesterday()
+    user = initialUser(monitor_user,yesterdayDate)
+    #twint.run.Search(user)
+    twint.run.Search(user)
+    with open("newTweet.txt",'r') as new:
+        for line in new.readlines():
+            if line == '\n':
+                pass
+            else:
+                tempTweet = twitterInfo(line)
+                newTweet.tList.append(tempTweet)
+    return newTweet
+                # print("Address: {}, Date: {},Time: {}, Timezone: {}, Username: {},Content: {},".format(Address,Date,Time,TimeZone,UserName,Content))
+
+def get_old_twitter():
+    # Read file
+    oldTweet = twitterList([])
+    with open("oldTweet.txt",'r') as new:
+        for line in new.readlines():
+            if line == '\n' or line == '':
+                pass
+            else:
+                tempTweet = twitterInfo(line)
+                oldTweet.tList.append(tempTweet)
+    return oldTweet
+
+def main():
+    oldTweet = get_old_twitter()
+    newTweet = get_new_twitter()
+    flag = False
+    #Have Something New
+    if (not oldTweet.tList) or (not newTweet.tList) or (oldTweet.tList[-1].address != newTweet.tList[-1].address):
+    #     # 返回一个变更
+        flag = True
+        oldTweet = newTweet
+        os.system("rm oldTweet.txt")
+        os.system("mv newTweet.txt oldTweet.txt")
+    #     # newTweet -> oldTweet
+    #     pass
+    else:
+        os.system("rm newTweet.txt")
+    return flag,oldTweet
+    #     # silence & keep
+    #     pass
+    # get_new_twitter()
+
+if __name__ == '__main__':
+    #flag = F 没更新 flag = T 更新了
+    flag, oldTweet = main()
+    #print(flag, oldTweet)
--- plugins/twint_scratcher.py
+++ plugins/twint_scratcher.py
@@ -1,5 +1,7 @@
 from nonebot import *
 from utils import *
-
+import os
 
 @on_command('refresh',aliases = ('刷新',))
+async def run_scratch():
+    os.system("python3 ../monitor.py")
