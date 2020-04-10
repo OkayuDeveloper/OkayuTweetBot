2b63e1cbfed1a7e0867d2547de192f99231f9f99
Marlon <1362941473@qq.com>
2020-04-10 23:12:00 +0800

    merge

--- connection.py
+++ connection.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     nonebot.init(config)
     nonebot.load_builtin_plugins()
     nonebot.load_plugins(path.join(path.dirname(__file__), 'plugins'), 'plugins')
-    nonebot.run(host='127.0.0.1', port = 8080)
+    nonebot.run(host='127.0.0.1', port = 8087)
